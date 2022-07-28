7f6f9ca0f58e642041e4c057295d897c757963df
Terra <bclaycamp@gmail.com>
2022-07-28 16:08:51 -0400

    fixed bug and retooled the hierarchy

--- src/App.js
+++ src/App.js
@@ -2,57 +2,48 @@ import "./App.css";
 import SearchForm from "./Components/SearchForm.jsx";
 import ListOfCities from "./Components/ListOfCitys";
 import React, { useState } from "react";
-import CityTile from "./Components/CityTile";
-import { GKYplanetPicker } from "./Functions/GKYplanetPicker";
 
 function App() {
   const [cityList, setCityList] = useState([]);
   const weatherAPIkey = "5d578c737ce21d8b0f9dd6879574a1b6";
-
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`
   const handleSelfDestruct = (cityKeyForDemo) => {
     console.log("X button pressed on", cityKeyForDemo);
-    console.log("this is cityList", cityList.key);
-
-    // setCityList(
-    //   cityList.filter((city) => {
-    //     console.log(city);
-    //     console.log("city.key", city.key,
-    //     "cityKeyForDemo", cityKeyForDemo);
-
-    //     console.log(city.props.key !== cityKeyForDemo);
-    //     return city.props.key !== cityKeyForDemo;
-    //   })
-    // );
+    setCityList(
+      cityList.filter((city) => {
+        return city.name + city.sys.country !== cityKeyForDemo;
+      })
+    );
   };
-
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`
   const searchBoxReset = () => {
     document.getElementById("searchBox").value = "";
     // document.getElementById("searchBox").focus();
   };
-
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`
   const search = async (inputCity) => {
     if (cityList.length > 0) {
-      const filteredArray = cityList.filter((el) => {
+      const filteredArray = cityList.filter((city) => {
         let content;
         if (inputCity.includes(",")) {
           if (inputCity.split(",")[1].length > 2) {
             inputCity = inputCity.split(",")[0];
-            content = el.props.cityName.toLowerCase();
+            content = city.name.toLowerCase();
           } else {
-            content = el.props.cityName.toLowerCase();
+            content = city.name.toLowerCase();
           }
         } else {
-          console.log(el);
-          console.log(el.props.cityName);
-          content = el.props.cityName.toLowerCase();
+          console.log(city);
+          console.log(city.name);
+          content = city.name.toLowerCase();
         }
-        return content == inputCity.toLowerCase();
+        return content === inputCity.toLowerCase();
       });
 
       // console.log(filteredArray);
       if (filteredArray.length > 0) {
         alert(
-          `You already know the weather for ${filteredArray[0].props.cityName}...otherwise please be more specific by providing the country code as well 😉`
+          `You already know the weather for ${filteredArray[0].name}...otherwise please be more specific by providing the country code as well 😉`
         );
         searchBoxReset();
         return;
@@ -70,37 +61,19 @@ function App() {
       })
       .then((data) => {
         console.log(data);
-        const { main, name, sys, weather } = data;
-        const icon = `https://openweathermap.org/img/wn/${weather[0]["icon"]}@2x.png`;
-        const GKYplanet = GKYplanetPicker(
-          Math.round(main.temp),
-          Math.round(main.humidity)
-        );
+        console.log("data.cod=", data.cod);
 
-        const newCity = (
-          <CityTile
-            cityName={name}
-            country={sys.country}
-            temp={main.temp}
-            humidity={main.humidity}
-            currentCondition={weather[0]["description"]}
-            weatherIcon={icon}
-            gkyPlanetName={GKYplanet[0]}
-            gkyPlanetImg={GKYplanet[1]}
-            gkyPlanetQuip={GKYplanet[2]}
-            key={name + sys.country}
-            keyVal={name + sys.country}
-            handleSelfDestruct={handleSelfDestruct}
-          />
-        );
-
-        // console.log( setCityList([...cityList, newCity]));
-        setCityList([...cityList, newCity]);
+          if (data.cod === '404') {
+            throw Error(data.message);
+          }
+        setCityList([...cityList, data]);
         searchBoxReset();
+
+
       })
       .catch((error) => {
         window.alert(
-          "Please enter a valid city. You may need to search for a nearby city if we can't find yours specifically."
+          "It looks like we couldn't find that one. You may need to search for a nearby city if we can't find yours specifically."
         );
         searchBoxReset();
       });
@@ -111,7 +84,10 @@ function App() {
       <h1 className="heading">Geeky Weather</h1>
       <SearchForm search={search} />
       <br />
-      <ListOfCities cityList={cityList} />
+      <ListOfCities
+        cityList={cityList}
+        handleSelfDestruct={handleSelfDestruct}
+      />
     </div>
   );
 }
--- src/Components/ListOfCitys.jsx
+++ src/Components/ListOfCitys.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { GKYplanetPicker } from "../Functions/GKYplanetPicker";
+import CityTile from "./CityTile";
 
-const ListOfCitys = ({ cityList }) => {
-  return <ul className="list-of-cities">{cityList}</ul>;
+const ListOfCitys = ({ cityList, handleSelfDestruct }) => {
+  return (
+    <ul className="list-of-cities">
+      {cityList.map((city) => {
+        const { main, name, sys, weather } = city;
+        const icon = `https://openweathermap.org/img/wn/${weather[0]["icon"]}@2x.png`;
+        const GKYplanet = GKYplanetPicker(
+          Math.round(main.temp),
+          Math.round(main.humidity)
+        );
+        return (
+          <CityTile
+            cityName={name}
+            country={sys.country}
+            temp={main.temp}
+            humidity={main.humidity}
+            currentCondition={weather[0]["description"]}
+            weatherIcon={icon}
+            gkyPlanetName={GKYplanet[0]}
+            gkyPlanetImg={GKYplanet[1]}
+            gkyPlanetQuip={GKYplanet[2]}
+            key={name + sys.country}
+            keyVal={name + sys.country}
+            handleSelfDestruct={handleSelfDestruct}
+          />
+        );
+      })}
+    </ul>
+  );
 };
 
 export default ListOfCitys;
